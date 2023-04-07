343fc6c41c0dd07d49b2ff6c4fbd2007aa39a02b
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-04-07 11:12:49 +0000

    [add]フラッシュメッセージ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,6 +8,7 @@ class ListsController < ApplicationController
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "投稿が成功しました。"
       redirect_to list_path(@list.id)
     else
       render :new
